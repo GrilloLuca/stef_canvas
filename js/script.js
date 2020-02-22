df0b37d0a176c90147ef644e806d997d744a7b74
lucagrillo_sxc <luca.grillo@sixthcontinent.com>
2020-02-22 15:52:42 +0100

    added more glitch effects

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 var canvas;
 var ctx;
 var img = new Image;
+var x = 30
+var y = 10
+var copy
 // img.crossOrigin = "Anonymous";
 
 window.onload = () => {
 
-    img.src = 'img/xcvbnm.jpg';
+    img.src = 'img/stef2.jpg';
 
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
@@ -13,17 +16,20 @@ window.onload = () => {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
     img.onload = drawimage
 
-    canvas.addEventListener('mousemove', onClick);
+    canvas.addEventListener('mousemove', executeGlitch);
+    // setInterval(executeGlitch, 100)
 }
 
-var onClick = (e) => {
-    // console.log(e)
-    var x = e.clientX;
-    var y = e.clientY;
+var executeGlitch = (e) => {
+    if(e != undefined) {
+        x = e.clientX;
+        y = e.clientY;
+    }
     
     var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
     // invertColors(imageData.data)
-    glitch5(imageData, x, y)
+    glitch7(imageData)
 
     ctx.putImageData(imageData, 0, 0)
 }
@@ -35,18 +41,31 @@ var drawimage = () => {
 
     ctx.drawImage(img, 0, 0);
 
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    // deep clone the image
+    copy = JSON.parse(JSON.stringify(imageData.data))
+
 }
 
-var glitch5 = (imageData, x, y) => {
-    var r = 4 * parseInt(Math.random()*2)+1
-    let perc = 4 * (parseInt(x/imageData.width*10)+1)
-
-    imageData.data.forEach((c, i) => {
-        imageData.data[i] = i%r == 3 ? c : imageData.data[i+perc]
+var glitch7 = (imageData) => {
+    imageData.data.forEach((b, i) => {
+        imageData.data[i] = i%4 == 3 ? b : copy[i^x]
     });
 }
 
-var glitch4 = (imageData, x, y) => {
+var glitch5 = (imageData) => {
+    
+    let percy = 1// 1, 2, 5, (y % 4) + 1
+
+    var r = 4 * parseInt(Math.random() * 2) + percy
+    let percx = 4 * (parseInt(x / imageData.width * 10) + 1)
+
+    imageData.data.forEach((b, i) => {
+        imageData.data[i] = i%r == 3 ? b : imageData.data[i+percx]
+    });
+}
+
+var glitch4 = (imageData) => {
     
     let perc = parseInt(x/imageData.width*20)+1
 
@@ -55,7 +74,7 @@ var glitch4 = (imageData, x, y) => {
     });
 }
 
-var glitch3 = (imageData, x, y) => {
+var glitch3 = (imageData) => {
     var r = 4 * parseInt(Math.random()*2)+1
     let perc = parseInt(x/imageData.width*10)+1
 
@@ -64,7 +83,7 @@ var glitch3 = (imageData, x, y) => {
     });
 }
 
-var glitch2 = (data, x, y) => {
+var glitch2 = (data) => {
     for(w = 0; w< data.width; w++) {
         for(h = 0; h< data.height; h++) {
             let r = 4 * parseInt(Math.random()*10)+1
