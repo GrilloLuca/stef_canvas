a814766e3ed8c47434fe3001c1fbfb0bbce4b66f
lucagrillo_sxc <luca.grillo@sixthcontinent.com>
2020-03-02 00:24:41 +0100

    first webgl glitch and code cleanin

--- js/script.js
+++ js/script.js
@@ -1,94 +1,45 @@
-import imageAsset from "../img/test.png";
-// import _ from "lodash"
-// import {glitch2, glitch3, glitch4, glitch5, glitch7, invertColors} from "./effects"
+import imageAsset from "../img/stef2.jpg";
+const regl = require("regl")();
 
-// console.log(imageAsset)
+var x = 0, y = 0
 
-// var canvas;
-// var ctx;
 var img = new Image();
-// var copy
-
-var loadImageBuffer = () => {
-  //   canvas.height = img.height;
-  //   canvas.width = img.width;
-
-  //   ctx.drawImage(img, 0, 0);
-
-  //   var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  // deep clone the image
-  //   copy = _.cloneDeep(imageData.data);
-
-  initRegl();
-};
-
-// var executeGlitch = (e) => {
-//     let x, y
-//     if(e != undefined) {
-//         x = e.clientX;
-//         y = e.clientY;
-//     }
-
-//     var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-//     // invertColors(imageData.data)
-//     glitch5(imageData, copy, x, y)
-
-//     ctx.putImageData(imageData, 0, 0)
-// }
-
-// var setPixel = (imageData, x, y, p) => {
-//     imageData.data[((y * (imageData.width * 4)) + (x * 4))] = p.r
-//     imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 1] = p.g
-//     imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 2] = p.b
-// }
-// var getPixel = (imageData, x, y) => {
-//     return {
-//         r: imageData.data[((y * (imageData.width * 4)) + (x * 4))],
-//         g: imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 1],
-//         b: imageData.data[((y * (imageData.width * 4)) + (x * 4)) + 2]
-//     }
-// }
 
 var init = () => {
+
   img.src = imageAsset;
 
-  //   canvas = document.getElementById("canvas");
-  //   ctx = canvas.getContext("2d");
-
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
-  img.onload = loadImageBuffer;
+  img.onload = initRegl;
   img.onerror = console.log;
 
-  // canvas.addEventListener('mousemove', executeGlitch);
-  // setInterval(executeGlitch, 100)
+  setInterval(initRegl, 50)
+
 };
 
 init();
 
 function initRegl() {
-  const regl = require("regl")();
 
   regl({
     frag: `
       precision mediump float;
       uniform sampler2D texture;
       uniform vec2 iResolution;
+      uniform float x;
+      uniform float y;
       
       varying vec2 uv;
       void main () {
-        vec4 color = texture2D(texture, uv);
+        vec4 color = texture2D(texture, uv);        
 
-        color.r = 1.0;
+        // color.r = 1.0;
 
-        //imageData.data[i] = i%4 == 3 ? b : copy[i^(4*x)]
+        // imageData.data[i] = i%4 == 3 ? b : copy[i^(4*x)]
 
         vec2 st = gl_FragCoord.xy/iResolution;
 
-        if( mod(st.x, 0.3) >= 0.1 ) {
-            //gl_FragColor = vec4(st.x, 0.0, 0.0, 1.0);
-            // color = texture2D(texture, uv * 1.0);
-            vec2 uvGlitch = uv - vec2(0.0, 0.01);
+        if( mod(st.y, ${Math.random()}) >= ${Math.random()} ) {
+            vec2 uvGlitch = uv - vec2(${Math.random()}, 0.01);
             color = texture2D(texture, uvGlitch);
             gl_FragColor = color;
         } else {
@@ -112,6 +63,7 @@ function initRegl() {
 
     uniforms: {
       texture: regl.texture(img),
+      x, y,
       iResolution: ctx => {
         return [ctx.viewportWidth, ctx.viewportHeight];
       }
